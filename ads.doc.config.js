309610aacd2f4e13cae3eddb7d1d34ccdc34469d
jinyang <961284688@qq.com>
2021-06-03 00:21:30 +0800

    feat: init

---
+++ ads.doc.config.js
@@ -0,0 +1,33 @@
+/*
+ * @Author: 锦阳
+ * @Create: 2021年05月31日
+ */
+const GenDoc = require('@ads/cli-plugin-doc');
+const fs = require('fs-extra');
+const { FastPath } = require('@ads/node-utils');
+module.exports = (
+    /**
+     * 配置参数
+     *
+     * @returns {GenDoc.RenderOptions}
+     */
+    async () => {
+        const [source] = (await Promise.all([
+            GenDoc.getFilesCode({ dir: './lib', files: ['*'] }),
+        ]));
+        return {
+            output: 'README.md',
+            helpers: {
+                devInstall: true,
+                importCode: '// tsconfig.json\n' + fs.readFileSync(FastPath.getCwdPath('./docs/import.js'), { encoding: 'utf-8' }),
+                remark: fs.readFileSync(FastPath.getCwdPath('./docs/usage.md'), { encoding: 'utf-8' }),
+                postfixes: [
+                    {
+                        id: 'source',
+                        title: '配置源码',
+                        content: GenDoc.renderCode(source),
+                    },
+                ],
+            },
+        };
+    })();
---
+++ commitlint.config.js
@@ -0,0 +1,7 @@
+/*
+ * @Author: 锦阳
+ * @Create: 2021年04月23日
+ */
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+};
